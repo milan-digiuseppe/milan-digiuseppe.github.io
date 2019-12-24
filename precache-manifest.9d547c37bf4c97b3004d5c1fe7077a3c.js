0ca91547df0cda6219067580b572b25b34432ec2
Milan John Paul Digiuseppe <mjpdigiuseppe@uwaterloo.ca>
2019-12-24 16:02:07 -0500

    Updates

---
+++ precache-manifest.9d547c37bf4c97b3004d5c1fe7077a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6ab71c31c1e880892a2a5b993e9cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b3dfdbf893c513fe0bf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "28f0cee1368c67f2ab67",
+    "url": "/static/js/2.bac23c3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bac23c3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b3dfdbf893c513fe0bf",
+    "url": "/static/js/main.7e9f675f.chunk.js"
+  },
+  {
+    "revision": "3044f18d10a9bb3115fb",
+    "url": "/static/js/runtime-main.68938cae.js"
+  }
+]);
